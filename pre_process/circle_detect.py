f4f6501d75a997f79f970b2e1f40a594a259eef5
sunary <v2nhat@gmail.com>
2015-10-27 13:38:01 +0700

    using hough transform detect line and circle

--- pre_process/circle_detect.py
+++ pre_process/circle_detect.py
@@ -1,14 +1,48 @@
 __author__ = 'sunary'
 
 
+import math
+
 class CircleDetect():
 
     def __init__(self):
         pass
 
     def process(self, pix):
-        pass
+        max_r = min(len(pix), len(pix[0]))/2 + 1
+        self.histogram = [[[0]* max_r for _ in range(len(pix[0]))] for _ in range(len(pix))]
+        for i in range(len(pix)):
+            for j in range(len(pix[0])):
+                for a in range(len(pix)):
+                    for b in range(len(pix[0])):
+                        r = int(math.sqrt((i - a)**2 + (j - b)**2))
+                        if r and r < max_r:
+                            self.histogram[i][j][r] += 1
+
+        max_rate = 0
+        save_a = 0
+        save_b = 0
+        save_r = 0
+        for i in range(0, len(self.histogram)):
+            for j in range(0, len(self.histogram[0])):
+                for r in range(0, len(self.histogram[0][0])):
+                    if self.histogram[i][j][r] > max_rate:
+                        max_rate = self.histogram[i][j][r]
+                        save_a = i
+                        save_b = j
+                        save_r = r
+
+        return save_a, save_b, save_r
 
 
 if __name__ == '__main__':
     circle_detect = CircleDetect()
+    pix = [[0, 0, 1, 0, 0, 0, 0, 0],
+           [0, 1, 0, 1, 0, 0, 0, 0],
+           [1, 1, 0, 1, 0, 0, 0, 0],
+           [0, 1, 0, 1, 0, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0]]
+    print circle_detect.process(pix)
--- pre_process/line_detect.py
+++ pre_process/line_detect.py
@@ -1,14 +1,49 @@
 __author__ = 'sunary'
 
 
+import math
+from pprint import pprint
+
+
 class LineDetect():
 
     def __init__(self):
-        pass
+        self.cos_angle = [math.cos(x * math.pi/180) for x in range(180)]
+        self.sin_angle = [math.sin(x * math.pi/180) for x in range(180)]
+
 
     def process(self, pix):
-        pass
+        self.histogram = [[0]*180 for _ in range(2*(len(pix) + len(pix[0])))]
+
+        for i in range(0, len(pix)):
+            for j in range(0, len(pix[0])):
+                if pix[i][j]:
+                    for angle in range(0, 180):
+                        d = i * self.cos_angle[angle] + j * self.sin_angle[angle]
+                        self.histogram[int(d + len(pix) + len(pix[0]))][angle] += 1
+
+        max_rate = 0
+        save_r = 0
+        save_angle = 0
+        for i in range(0, len(self.histogram)):
+            for j in range(0, len(self.histogram[0])):
+                if self.histogram[i][j] > max_rate:
+                    max_rate = self.histogram[i][j]
+                    save_r = i
+                    save_angle = j
+
+        # y = -cos(a)*x/sin(a) + r/sin(a)
+        return save_r - len(pix) - len(pix[0]), save_angle
 
 
 if __name__ == '__main__':
     line_detect = LineDetect()
+    pix = [[0, 0, 0, 0, 0, 0, 0, 0],
+           [1, 0, 0, 0, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0, 0],
+           [0, 0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 0, 0, 0, 1, 0]]
+    print line_detect.process(pix)
