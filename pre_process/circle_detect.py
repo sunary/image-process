2915d6859a577f7f878777c8a7f516574d1ce740
sunary <v2nhat@gmail.com>
2015-11-05 10:10:06 +0700

    hough tranforms

--- pre_process/circle_detect.py
+++ pre_process/circle_detect.py
@@ -3,6 +3,7 @@ __author__ = 'sunary'
 
 import math
 
+
 class CircleDetect():
 
     def __init__(self):
@@ -11,11 +12,12 @@ class CircleDetect():
     def process(self, pix):
         max_r = min(len(pix), len(pix[0]))/2 + 1
         self.histogram = [[[0]* max_r for _ in range(len(pix[0]))] for _ in range(len(pix))]
+
         for i in range(len(pix)):
             for j in range(len(pix[0])):
-                for a in range(len(pix)):
-                    for b in range(len(pix[0])):
-                        r = int(math.sqrt((i - a)**2 + (j - b)**2))
+                for a in range(i, len(pix)):
+                    for b in range(j, len(pix[0])):
+                        r = int(round(math.sqrt((i - a)**2 + (j - b)**2)))
                         if r and r < max_r:
                             self.histogram[i][j][r] += 1
 
@@ -23,15 +25,16 @@ class CircleDetect():
         save_a = 0
         save_b = 0
         save_r = 0
-        for i in range(0, len(self.histogram)):
-            for j in range(0, len(self.histogram[0])):
-                for r in range(0, len(self.histogram[0][0])):
+        for i in range(len(self.histogram)):
+            for j in range(len(self.histogram[0])):
+                for r in range(len(self.histogram[0][0])):
                     if self.histogram[i][j][r] > max_rate:
                         max_rate = self.histogram[i][j][r]
                         save_a = i
                         save_b = j
                         save_r = r
 
+        #(x - a)^2 + (y - b)^2 = r^2
         return save_a, save_b, save_r
 
 
--- pre_process/line_detect.py
+++ pre_process/line_detect.py
@@ -2,7 +2,6 @@ __author__ = 'sunary'
 
 
 import math
-from pprint import pprint
 
 
 class LineDetect():
@@ -11,29 +10,28 @@ class LineDetect():
         self.cos_angle = [math.cos(x * math.pi/180) for x in range(180)]
         self.sin_angle = [math.sin(x * math.pi/180) for x in range(180)]
 
-
     def process(self, pix):
         self.histogram = [[0]*180 for _ in range(2*(len(pix) + len(pix[0])))]
 
-        for i in range(0, len(pix)):
-            for j in range(0, len(pix[0])):
+        for i in range(len(pix)):
+            for j in range(len(pix[0])):
                 if pix[i][j]:
-                    for angle in range(0, 180):
+                    for angle in range(180):
                         d = i * self.cos_angle[angle] + j * self.sin_angle[angle]
-                        self.histogram[int(d + len(pix) + len(pix[0]))][angle] += 1
+                        self.histogram[int(round(d)) + (len(pix) + len(pix[0]))][angle] += 1
 
         max_rate = 0
         save_r = 0
         save_angle = 0
-        for i in range(0, len(self.histogram)):
-            for j in range(0, len(self.histogram[0])):
+        for i in range(len(self.histogram)):
+            for j in range(len(self.histogram[0])):
                 if self.histogram[i][j] > max_rate:
                     max_rate = self.histogram[i][j]
                     save_r = i
                     save_angle = j
 
         # y = -cos(a)*x/sin(a) + r/sin(a)
-        return save_r - len(pix) - len(pix[0]), save_angle
+        return save_r - (len(pix) + len(pix[0])), save_angle
 
 
 if __name__ == '__main__':
