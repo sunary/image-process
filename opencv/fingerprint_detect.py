9f6dced80f43d903823850c9e41a048d2974e457
sunary <v2nhat@gmail.com>
2015-11-06 16:13:40 +0700

    using threshold

--- opencv/fingerprint_detect.py
+++ opencv/fingerprint_detect.py
@@ -13,32 +13,47 @@ class FingerprintDetect():
     def __init__(self):
         pass
 
-    def process(self, img_file):
+    def convert_gray(self, img_file):
         edge_detect = EdgeDetect()
         pix = cv2.imread(os.path.dirname(__file__) + img_file, 0)
-        # pix = helper.read_image(os.path.dirname(__file__) + img_file)
-        # pix = helper.convert_gray(pix)
-        # pix = np.array(pix)
 
-        pix_none = pix.T
-        helper.save_image(pix_none, os.path.dirname(__file__) + '/../resources/gray2.png', True)
+        pix_none = pix
         pix_none = edge_detect.process(pix_none)
-        helper.save_image(pix_none, os.path.dirname(__file__) + '/../resources/edg2e.png')
+        cv2.imshow('pix-none', np.array(pix_none))
 
         # equalize histogram
-        pix_equal = cv2.equalizeHist(pix.T)
-        helper.save_image(pix_equal, os.path.dirname(__file__) + '/../resources/gray_equal2.png', True)
+        pix_equal = cv2.equalizeHist(pix)
         pix_equal = edge_detect.process(pix_equal)
-        helper.save_image(pix_equal, os.path.dirname(__file__) + '/../resources/edge_equal2.png')
+        cv2.imshow('pix-equal', np.array(pix_equal))
 
         # Contrast Limited Adaptive Histogram Equalization
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-        pix_clahe = clahe.apply(pix.T)
-        helper.save_image(pix_clahe, os.path.dirname(__file__) + '/../resources/gray_clahe2.png', True)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
+        pix_clahe = clahe.apply(pix)
+        cv2.imshow('pix-clahe-gray', np.array(pix_clahe))
         pix_clahe = edge_detect.process(pix_clahe)
-        helper.save_image(pix_clahe, os.path.dirname(__file__) + '/../resources/edge_clahe2.png')
+        cv2.imshow('pix-clahe', np.array(pix_clahe))
+        pix_threshold = cv2.adaptiveThreshold(np.array(pix_clahe) , 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 4)
+        cv2.imshow('pix-threshold', pix_threshold)
 
+        cv2.waitKey()
+
+    def convert_hsv(self, img_file):
+        pix = cv2.imread(os.path.dirname(__file__) + img_file)
+
+        # using HSV space
+        hsv = cv2.cvtColor(pix, cv2.COLOR_BGR2HSV)
+        pix_s = hsv[:, :, 1]
+        cv2.imshow("pix-s", pix_s)
+
+        # Adaptive Threshold -> CLAHE
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
+        pix_clahe = clahe.apply(pix_s)
+        pix_threshold = cv2.adaptiveThreshold(pix_clahe , 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 4)
+        cv2.imshow("clare-4", pix_threshold)
+
+        cv2.waitKey()
 
 if __name__ == '__main__':
     fp_detect = FingerprintDetect()
-    pix = fp_detect.process('/../resources/fp02.jpg')
+    fp_detect.convert_gray('/../resources/fp02.jpg')
+    fp_detect.convert_hsv('/../resources/fp02.jpg')
