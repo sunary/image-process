cf9992c422abe5de6bbad6707024ab7248ec19e6
sunary <v2nhat@gmail.com>
2015-11-18 17:41:21 +0700

    ostu threshold

--- opencv/fingerprint_detect.py
+++ opencv/fingerprint_detect.py
@@ -6,7 +6,8 @@ import os
 import cv2
 import numpy as np
 from pre_process.edge_detect import EdgeDetect
-from pre_process.orientation_field import OrientationField
+from pre_process import threshold
+from pre_process.fingerprint_enhance import FingerprintEnhance
 
 
 class FingerprintDetect():
@@ -40,29 +41,6 @@ class FingerprintDetect():
 
         cv2.waitKey()
 
-    def ostu_threshold(self, img, blursize):
-        blur = cv2.GaussianBlur(img,(blursize,blursize),0)
-        hist = cv2.calcHist([blur],[0],None,[256],[0,256])
-        hist_norm = hist.ravel()/hist.max()
-        Q = hist_norm.cumsum()
-        bins = np.arange(256)
-        fn_min = np.inf
-        thresh = -1
-        for i in xrange(1,256):
-            p1,p2 = np.hsplit(hist_norm,[i]) # probabilities
-            q1,q2 = Q[i],Q[255]-Q[i] # cum sum of classes
-            b1,b2 = np.hsplit(bins,[i]) # weights
-            if q1==0: continue
-            if q2==0: continue
-            m1,m2 = np.sum(p1*b1)/q1, np.sum(p2*b2)/q2
-            v1,v2 = np.sum(((b1-m1)**2)*p1)/q1,np.sum(((b2-m2)**2)*p2)/q2
-            fn = v1*q1 + v2*q2
-            if fn < fn_min:
-                fn_min = fn
-                thresh = i
-        ret, otsu = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        return otsu
-
     def convert_hsv(self, img_file):
         pix = cv2.imread(os.path.dirname(__file__) + img_file)
 
@@ -70,25 +48,25 @@ class FingerprintDetect():
         c_size = r_size - 1
 
         # using HSV space
-        hsv = cv2.cvtColor(pix, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(pix, cv2.COLOR_BGR2HLS)
         pix_s = hsv[:, :, 1]
         cv2.imshow('pix-s', pix_s)
         # Adaptive Threshold -> CLAHE
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(c_size, c_size))
         pix_clahe = clahe.apply(pix_s)
-        pix_bin = cv2.adaptiveThreshold(pix_clahe, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,
+        pix_bin = cv2.adaptiveThreshold(pix_clahe, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,
                                         blockSize=r_size*2 + 1, C=r_size)
         cv2.imshow('pix-bin', pix_bin)
-        pix_threshold = self.ostu_threshold(pix_bin, r_size - 2)
+        pix_threshold = threshold.ostu_algorithm(pix_bin, r_size - 2)
         cv2.imshow('pix-ostu', pix_threshold)
 
-        # orientation = OrientationField()
-        # pix_enhance = orientation.fingerprint_enhance(pix_threshold)
+        # fe = FingerprintEnhance()
+        # pix_enhance = fe.gabor_filter(pix_threshold)
         # cv2.imshow("pix-enhance", np.array(pix_enhance))
 
         cv2.waitKey()
 
 if __name__ == '__main__':
     fp_detect = FingerprintDetect()
-    # fp_detect.convert_gray('/../resources/fp02.jpg')
-    fp_detect.convert_hsv('/../resources/fp02.jpg')
+    # fp_detect.convert_gray('/../resources/fp03.jpg')
+    fp_detect.convert_hsv('/../resources/fp03.jpg')
--- pre_process/orientation_field.py
+++ pre_process/orientation_field.py
@@ -2,131 +2,12 @@ __author__ = 'sunary'
 
 
 import math
-import numpy as np
 
 
 class OrientationField():
 
     def __init__(self):
         pass
-
-    def fingerprint_enhance(self, pix):
-        print("fingerprint enhance calculator")
-        block_size = (9, 9)
-
-        dx = [[0] * len(pix[0]) for _ in range(len(pix))]
-        dy = [[0] * len(pix[0]) for _ in range(len(pix))]
-
-        self._sobel()
-
-        for i in range(len(self.gx)/2, len(pix) - len(self.gx)/2):
-            for j in range(len(self.gy)/2, len(pix[0]) - len(self.gy)/2):
-
-                for d1 in range(len(self.gx)):
-                    for d2 in range(len(self.gx)):
-                        dx[i][j] += self.gx[d1][d2] * pix[i + d1 - len(self.gx)/2][j + d2 - len(self.gx)/2]
-                        dy[i][j] += self.gy[d1][d2] * pix[i + d1 - len(self.gx)/2][j + d2 - len(self.gy)/2]
-
-        vx = [[2 * dx[i][j] * dy[i][j] for j in range(len(pix[0]))] for i in range(len(pix))]
-        vy = [[dx[i][j]**2 - dy[i][j]**2 for j in range(len(pix[0]))] for i in range(len(pix))]
-
-        vx_integral_img = self.cal_integral_img(vx)
-        vy_integral_img = self.cal_integral_img(vy)
-
-        orientation = [[0] * len(pix[0]) for _ in range(len(pix))]
-        for i in range(len(pix)):
-            for j in range(len(pix[0])):
-                min_i, max_i, min_j, max_j = self.ij_value((len(pix), len(pix[0])), block_size, i, j)
-
-                _vx = vx_integral_img[max_i][max_j] + vx_integral_img[min_i][min_j] -\
-                      (vx_integral_img[max_i][min_j] + vx_integral_img[min_i][max_j])
-                _vy = vy_integral_img[max_i][max_j] + vy_integral_img[min_i][min_j] -\
-                      (vy_integral_img[max_i][min_j] + vy_integral_img[min_i][max_j])
-
-                orientation[i][j] = math.atan2(_vy, _vx)/2
-
-        integral_orientation = self.cal_integral_img(orientation)
-        mean_orientation = [[0] * len(pix[0]) for _ in range(len(pix))]
-        for i in range(len(pix)):
-            for j in range(len(pix[0])):
-                min_i, max_i, min_j, max_j = self.ij_value((len(pix), len(pix[0])), block_size, i, j)
-
-                mean_orientation[i][j] = 1.*(integral_orientation[max_i][max_j] + integral_orientation[min_i][min_j] -\
-                      (integral_orientation[max_i][min_j] + integral_orientation[min_i][max_j]))/\
-                      (block_size[0] * block_size[1])
-
-        variance_orientation = [[0] * len(pix[0]) for _ in range(len(pix))]
-        for i in range(block_size[0]/2, len(pix) - block_size[0]/2):
-            for j in range(block_size[1]/2, len(pix[0]) - block_size[1]/2):
-
-                for m in range(-block_size[0]/2, block_size[0]/2 + 1):
-                    for n in range(-block_size[1]/2, block_size[1]/2 + 1):
-                        variance_orientation[i][j] += (mean_orientation[i][j] - orientation[i + m][j + n])**2
-
-                variance_orientation[i][j] = math.sqrt(variance_orientation[i][j] / (block_size[0] * block_size[1]))
-
-        confident = [[0] * len(pix[0]) for _ in range(len(pix))]
-        gen_pix = [[0xffffff] * len(pix[0]) for _ in range(len(pix))]
-        threshold = 0
-        conf_stat = []
-        for i in range(block_size[0]/2, len(pix) - block_size[0]/2):
-            for j in range(block_size[1]/2, len(pix[0]) - block_size[1]/2):
-                min_i, max_i, min_j, max_j = self.ij_value((len(pix), len(pix[0])), block_size, i, j)
-
-                dx = vx_integral_img[max_i][max_j] + vx_integral_img[min_i][min_j] -\
-                      (vx_integral_img[max_i][min_j] + vx_integral_img[min_i][max_j])
-                dy = vy_integral_img[max_i][max_j] + vy_integral_img[min_i][min_j] -\
-                      (vy_integral_img[max_i][min_j] + vy_integral_img[min_i][max_j])
-
-                memx = memy = 0
-
-                for _ in range(block_size[0]/2):
-                    confident[i][j] += self.gaussian_dist_angle(orientation[i + memx][j + memy], mean_orientation[i][j], variance_orientation[i][j])
-                    confident[i][j] += self.gaussian_dist_angle(orientation[i - memx][j - memy], mean_orientation[i][j], variance_orientation[i][j])
-                    confident[i][j] -= self.gaussian_dist_angle(orientation[i + memx][j - memy], mean_orientation[i][j], variance_orientation[i][j])
-                    confident[i][j] -= self.gaussian_dist_angle(orientation[i - memx][j + memy], mean_orientation[i][j], variance_orientation[i][j])
-
-                    if dx*memy < dy*memx:
-                        memy += 1
-                    else:
-                        memx += 1
-
-                conf_stat.append(confident[i][j])
-                if confident[i][j] > threshold:
-                    gen_pix[i][j] = 0
-
-        print np.histogram(conf_stat, 30)
-
-        return gen_pix
-
-    def cal_integral_img(self, pix):
-        '''
-        Examples:
-            >>> cal_integral_img([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
-            [[1, 3, 6], [2, 6, 12], [3, 9, 18]]
-        '''
-
-        integral_img = [[0] * len(pix[0]) for _ in range(len(pix))]
-
-        integral_img[0][0] = pix[0][0]
-        for j in range(1,len(pix[0])):
-            integral_img[0][j]=integral_img[0][j-1]+pix[0][j]
-
-        for i in range(1, len(pix)):
-            line_sum = 0
-            for j in range(len(pix[0])):
-                line_sum += pix[i][j]
-                integral_img[i][j] = integral_img[i-1][j] + line_sum
-
-        return integral_img
-
-    def ij_value(self, pix_size, block_size, i, j):
-        min_i = max(i - block_size[0]/2-1, 0)
-        max_i = min(i + block_size[0]/2, pix_size[0] - 1)
-        min_j = max(j - block_size[1]/2-1, 0)
-        max_j = min(j + block_size[1]/2, pix_size[1] - 1)
-
-        return min_i, max_i, min_j, max_j
 
     def process(self, pix):
         block_size = (15, 15)
@@ -147,9 +28,7 @@ class OrientationField():
         n_blocks_x = len(pix) / block_size[0]
         n_blocks_y = len(pix[0]) / block_size[1]
 
-        orientation = [[0] * len(pix[0]) for _ in range(len(pix))]
         block_orientation = [[0] * n_blocks_y for _ in range(n_blocks_x)]
-        block_variance = [[0] * n_blocks_y for _ in range(n_blocks_x)]
 
         for i in range(block_size[0]/2, len(pix) - block_size[0]/2):
             for j in range(block_size[1]/2, len(pix[0]) - block_size[1]/2):
@@ -161,70 +40,7 @@ class OrientationField():
                         sum_v1 += 2 * vx[i + ii][j + jj] * vy[i + ii][j + jj]
                         sum_v2 += vx[i + ii][j + jj] **2 - vy[i + ii][j + jj] **2
 
-                orientation[i][j] = math.atan2(sum_v2, sum_v1)/2
-
-        for m in range(len(block_orientation)):
-            for n in range(len(block_orientation[0])):
-                sum_v1 = 0.0
-                sum_v2 = 0.0
-
-                values = []
-                for i in range(block_size[0]):
-                    for j in range(block_size[1]):
-                        pix_i = min(m * len(block_orientation) + i, len(pix) - 1)
-                        pix_j = min(n * len(block_orientation[0]) + j, len(pix[0]) - 1)
-
-                        sum_v1 += 2 * vx[pix_i][pix_j] * vy[pix_i][pix_j]
-                        sum_v2 += vx[pix_i][pix_j] **2 - vy[pix_i][pix_j] **2
-                        values.append(orientation[m + i][n + j])
-
-                block_orientation[m][n] = math.atan2(sum_v2, sum_v1)/2
-                curr_var = 0
-                for phi in values:
-                    curr_var += ((phi - block_orientation[m][n] + 10 * math.pi) % math.pi) **2
-                block_variance[m][n] = curr_var
-
-        gen_pix = [[0xffffff] * len(pix[0]) for _ in range(len(pix))]
-        threshold = 0.02
-
-        ht_stat = []
-        hb_stat = []
-        value_Y = 100
-        for ii in range(len(pix)):
-            for jj in range(len(pix[0])):
-                m = min(ii / block_size[0], len(block_orientation) - 1)
-                n = min(jj / block_size[1], len(block_orientation[1]) - 1)
-
-                ro = [-math.sin(block_orientation[m][n])/2, math.sin(block_orientation[m][n])/2]
-                i = value_Y * ro[0]
-                j = value_Y * ro[1]
-                d = value_Y / (2* math.cos(block_orientation[m][n]))
-
-                l = int(j * math.tan(block_orientation[m][n]) + .5)
-                delta = block_variance[m][n]
-                ht = 0
-                hb = 0
-
-                if i == l:
-                    ht = self.gaussian_dist(delta)
-                elif i == l - d:
-                    ht = -self.gaussian_dist(delta)
-
-                if i == l:
-                    hb = self.gaussian_dist(delta)
-                elif i == l + d:
-                    hb = -self.gaussian_dist(delta)
-
-                if ht >= threshold and hb >= threshold:
-                    gen_pix[ii][jj] = 0
-
-                ht_stat.append(ht)
-                hb_stat.append(hb)
-
-        print np.histogram(ht_stat, 30)
-        print np.histogram(ht_stat, 30)
-
-        return gen_pix
+                block_orientation[i][j] = math.atan2(sum_v2, sum_v1)/2
 
     def _sobel(self):
         self.gx = [[-1, 0, 1],
@@ -234,39 +50,7 @@ class OrientationField():
                   [0, 0, 0],
                   [1, 2, 1]]
 
-        self.gx = [[1, 2, 0, -2, -1],
-                   [4, 8, 0, -8, -4],
-                   [6, 12, 0, -12, -6],
-                   [4, 8, 0, -8, -4],
-                   [1, 2, 0, -2, -1]]
-        self.gy = [[-1, -4, -6, -4, -1],
-                   [-2, -8, -12, -8, -2],
-                   [0, 0, 0, 0, 0],
-                   [2, 8, 12, 8, 2],
-                   [1, 4, 6, 4, 1]]
-
-    def gaussian_dist(self, x, mx=0, vx=1):
-        return math.exp(-.5 * ((x - mx) / vx) **2)
-
-    def gaussian_dist_angle(self, x, mx=0, vx=1):
-        x = x - mx
-        if x < math.pi:
-            x += 2 * math.pi
-        if x > math.pi:
-            x -= 2 * math.pi
-        return math.exp(-.5 * ((x - mx) / vx) **2)
-
 
 if __name__ == '__main__':
-    orientation = OrientationField()
-    pix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0],
-          [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-          [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-          [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
-          [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
-          [0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0],
-          [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
-    print orientation.fingerprint_enhance(pix)
+    of = OrientationField()
+    of.process()
